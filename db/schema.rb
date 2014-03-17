8a91d80ec9f42fae2335bdf524104cdc3993f7d9
Zach Klabunde <zbunde@gmail.com>
2014-03-17 10:13:00 -0600

    created new migration file for User with email_address changed to email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131218191546) do
+ActiveRecord::Schema.define(version: 20140317160910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20131218191546) do
     t.string   "password"
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email_address"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
